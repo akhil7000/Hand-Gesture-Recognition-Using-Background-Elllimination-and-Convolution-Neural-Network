092d02bd40270ebbfae39cef542db9f205c7d502
“Akhil <acherian@rccl.com>
2020-10-22 03:35:38 +0530

    added logic for eyes recognition

--- ResizeImages.py
+++ ResizeImages.py
@@ -5,11 +5,15 @@ def resizeImage(imageName):
     img = Image.open(imageName)
     wpercent = (basewidth/float(img.size[0]))
     hsize = int((float(img.size[1])*float(wpercent)))
-    img = img.resize((basewidth,hsize), Image.ANTIALIAS)
+    img = img.resize((89,100), Image.ANTIALIAS)
+    print(img.size)
     img.save(imageName)
 
-for i in range(0, 101):
+for i in range(0, 100):
     # Mention the directory in which you wanna resize the images followed by the image name
-    resizeImage("Dataset/FistTest/fist_" + str(i) + '.png')
+    resizeImage("Dataset/OpenEyesImages/Train/open_" + str(i) + '.png')
+    resizeImage("Dataset/OpenEyesImages/Test/open_" + str(i) + '.png')
+    resizeImage("Dataset/ClosedEyesImages/Train/closed_" + str(i) + '.png')
+    resizeImage("Dataset/ClosedEyesImages/Test/closed_" + str(i) + '.png')
 
 
